36e5618eff681a37997cf6d94f78447ff658b436
Everest <chandlerg.tech@gmail.com>
2026-03-20 17:30:36 -0400

    added dictionaryTable and printing its values

--- Task2_Data&Control.lua
+++ Task2_Data&Control.lua
@@ -1,13 +1,20 @@
 --lua only has one versatile data structure: Tables
 
-theUltimateTable = { 1, 2, 3, "hello, world!"}
+-- ultimate table holds integers, strings, and key-value pairs that act like a dictionary
+theUltimateTable = { 1, 2, 3, "hello, world!", key = "value" }
 lessUltimateTable = { "a", "b", "c" }
 numberTable = { 1, 2, 3, 4, 5 }
+dictionaryTable = { key1 = "value1", key2 = "value2", key3 = "value3" }
 
 -- # grabs the length of a table
 -- iterate through the table and print each value 
 for i = 1, #theUltimateTable do
     print(theUltimateTable[i])
+end
+
+-- iterate through the dictionaryTable and print each key and its value
+for key, value in pairs(dictionaryTable) do
+    print(key, value)
 end
 
 -- use a manual iteration with while loop to print the lessUltimateTable
